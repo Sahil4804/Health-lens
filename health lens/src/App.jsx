cef999ededf2b508202863274676d9788223a15b
Sahil4804 <sahil.patel@students.iiit.ac.in>
2024-03-24 16:48:44 +0530

    starter code

--- health lens/src/App.jsx
+++ health lens/src/App.jsx
@@ -1,33 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from "react";
+import * as d3 from "d3";
+
+import LineBarSeries from "./components/ChartBar/LineBarSeries";
+
+import ChartWithPanel from "./components/ChartPanel/ChartWithPanel";
+import { Provider } from "./components/ChartPanel/Provider";
+import { getData, getNewData } from "./utils/getData";
+
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const metricsAttrs = {position: 'top', value: "maximum", categories: {'heart_rate': 'Heart Rate', 'heart_intensity': 'Heart Intensity', 'blood_pulse': 'Blood Pulse'}}
+
+const App = () => {
+
+  const [datetime, setDateTime] = useState({'start': new Date(), 'current': new Date()});
+  const [data, setData] = useState(null)
+  const LineBarChart = ChartWithPanel((props) => <LineBarSeries {...props} />)
+  const [index, setIndex] = useState(0)
+  //const milliseconds = 60000 //this will update the chart every minute
+  const milliseconds = 1000 //this will update the chart every second (for testing purposes)
+  const timeFormat = milliseconds === 1000 ? d3.timeFormat("%H:%M:%S %p") : d3.timeFormat("%H:%M %p")
+
+  useEffect(() => {
+
+  }, [])
+
+  useEffect(() => {
+
+    setData(getData(milliseconds))
+
+    const interval = setInterval(() => {
+
+      const current = new Date()
+
+      let dataInitial = getData(milliseconds, index)
+      let dataToAdd = getNewData(current, milliseconds)
+      
+      setData(dataInitial)
+      setDateTime({'start': new Date(new Date().setHours(new Date().getHours() + dataInitial.hours)), 'current': current})
+
+      if(data){
+        setData({'heart': [{...data.heart, ...dataToAdd.hrNew}], 'blood_pulse': [{...data.blood_pulse, ...dataToAdd.bpNew}] }) 
+      }
+
+    }, milliseconds);
+    return () => clearInterval(interval);
+
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    {data && 
+    <div className="wrapper">
+      <div className="header">
+        <h2>MONITORING PATIENT VITALS</h2>
+        <div className='header-details'>
+          <p><span style={{fontWeight: 'bold'}}>Patient ID:</span> 352405</p>
+          <p><span style={{fontWeight: 'bold'}}>Datetime Range:</span> {d3.timeFormat("%B %d, %Y")(datetime.start)} {timeFormat(datetime.start) + ' - ' + timeFormat(datetime.current)}</p>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="box">
+        <Provider>
+        <LineBarChart
+          data={data} 
+          metrics={metricsAttrs}
+          timeformat={timeFormat}
+        />
+        </Provider>       
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </div>}
     </>
   )
 }
